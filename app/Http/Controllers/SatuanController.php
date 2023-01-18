55f6476a67ee176c92e1e5e9ff18a3a7bb83554e
handikaFadli <handikafadli23@gmail.com>
2023-01-18 23:15:01 +0700

    menambahkan fitur CRUD pada data satuan massa

--- app/Http/Controllers/SatuanController.php
+++ app/Http/Controllers/SatuanController.php
@@ -15,11 +15,11 @@ class SatuanController extends Controller
      */
     public function index()
     {
-        $satuan = Satuan::all();
+        $satuan = Satuan::paginate(3);
 
         // mengirim tittle dan judul ke view
         return view(
-            'satuan.index',
+            'pages.satuan.index',
             [
                 'satuan' => $satuan,
                 'tittle' => 'Data Satuan',
@@ -30,64 +30,64 @@ class SatuanController extends Controller
         );
     }
 
-    // public function store(Request $request)
-    // {
-    //     // mengubah error ke bahasa indonesia
-    //     $messages = [
-    //         'required' => ':attribute tidak boleh kosong',
-    //     ];
+    public function store(Request $request)
+    {
+        // mengubah error ke bahasa indonesia
+        $messages = [
+            'required' => ':attribute tidak boleh kosong',
+        ];
 
-    //     $errors = [
-    //         'nm_satuan' => 'Nama Satuan',
-    //     ];
+        $errors = [
+            'nm_satuan' => 'Nama Satuan',
+        ];
 
-    //     // validasi data
-    //     $request->validate([
-    //         'nm_satuan' => 'required',
-    //     ], $messages, $errors);
+        // validasi data
+        $request->validate([
+            'nm_satuan' => 'required',
+        ], $messages, $errors);
 
 
 
-    //     Satuan::create($request->all());
+        Satuan::create($request->all());
 
-    //     Alert::success('Data Satuan', 'Berhasil Ditambahkan!');
-    //     return redirect('satuan');
-    // }
+        Alert::success('Data Satuan', 'Berhasil Ditambahkan!');
+        return redirect('satuan');
+    }
 
-    // public function show(Satuan $satuan)
-    // {
-    //     //
-    // }
+    public function show(Satuan $satuan)
+    {
+        //
+    }
 
-    // public function edit(Satuan $satuan)
-    // {
-    //     return view(
-    //         'satuan.edit',
-    //         compact('satuan'),
-    //         [
-    //             'tittle' => 'Edit Data Satuan',
-    //             'judul' => 'Edit Data Satuan',
-    //             'menu' => 'Data Satuan',
-    //             'submenu' => 'Edit Data Satuan'
-    //         ]
-    //     );
-    // }
+    public function edit(Satuan $satuan)
+    {
+        return view(
+            'pages.satuan.edit',
+            compact('satuan'),
+            [
+                'tittle' => 'Edit Data Satuan',
+                'judul' => 'Edit Data Satuan',
+                'menu' => 'Data Satuan',
+                'submenu' => 'Edit Data Satuan'
+            ]
+        );
+    }
 
-    // public function update(Request $request, Satuan $satuan)
-    // {
-    //     $request->validate([
-    //         'nm_satuan' => 'required'
-    //     ]);
+    public function update(Request $request, Satuan $satuan)
+    {
+        $request->validate([
+            'nm_satuan' => 'required'
+        ]);
 
-    //     $satuan->update($request->all());
-    //     Alert::success('Data Satuan', 'Berhasil diubah!');
-    //     return redirect('satuan');
-    // }
+        $satuan->update($request->all());
+        Alert::success('Data Satuan', 'Berhasil diubah!');
+        return redirect('satuan');
+    }
 
-    // public function destroy(Satuan $satuan)
-    // {
-    //     $satuan->delete();
-    //     Alert::success('Data Satuan', 'Berhasil dihapus!');
-    //     return redirect('satuan');
-    // }
+    public function destroy(Satuan $satuan)
+    {
+        $satuan->delete();
+        Alert::success('Data Satuan', 'Berhasil dihapus!');
+        return redirect('satuan');
+    }
 }

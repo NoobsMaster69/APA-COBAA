4c91a9361cda09ad10abe1faf5a21ff011fec139
reno <ayamtiren54321@gmail.com>
2023-02-09 11:16:41 +0700

    lupa ngebenerin apa di landingpage

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -619,7 +619,6 @@
                         </div>
                         <ul class="contact-infor">
                             <li><strong>Bread Smile </strong> <span>perusahaan ini di bangun pada tahun 2006 oleh (Gak tawu siapa ) atas dasar kemanusiaan. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate iste reiciendis sed repellendus vel perspiciatis temporibus animi, laudantium pariatur rerum. Ab tempora non quasi praesentium consequatur. Illo non voluptates voluptatem.</span>
-
                         </ul>
                     </div>
                 </div>
@@ -628,37 +627,23 @@
             </div>
         </div>
     </section>
-    <!-- <div class="container pb-30 wow animate__animated animate__fadeInUp" data-wow-delay="0">
+    <div class="container pb-30 wow animate__animated animate__fadeInUp" data-wow-delay="0">
             <div class="row align-items-center">
                 <div class="col-12 mb-30">
                     <div class="footer-bottom"></div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-md-6">
-                    <p class="font-sm mb-0">&copy; 2022, <strong class="text-brand">Nest</strong> - HTML Ecommerce Template <br />All rights reserved</p>
+                    <p class="font-sm mb-0">&copy; 2022, <strong class="text-brand">Bread Smile</strong> - Toko Roti <br />All rights reserved</p>
                 </div>
                 <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
                     <div class="hotline d-lg-inline-flex mr-30">
                         <img src="assets/imgs/theme/icons/phone-call.svg" alt="hotline" />
-                        <p>1900 - 6666<span>Working 8:00 - 22:00</span></p>
-                    </div>
-                    <div class="hotline d-lg-inline-flex">
-                        <img src="assets/imgs/theme/icons/phone-call.svg" alt="hotline" />
-                        <p>1900 - 8888<span>24/7 Support Center</span></p>
+                        <p>1900 - 6666<span>Info Pemesanan</span></p>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-6 col-md-6 text-end d-none d-md-block">
-                    <div class="mobile-social-icon">
-                        <h6>Follow Us</h6>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-facebook-white.svg" alt="" /></a>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-twitter-white.svg" alt="" /></a>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-instagram-white.svg" alt="" /></a>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-pinterest-white.svg" alt="" /></a>
-                        <a href="#"><img src="assets/imgs/theme/icons/icon-youtube-white.svg" alt="" /></a>
-                    </div>
-                    <p class="font-sm">Up to 15% discount on your first subscribe</p>
-                </div>
+
             </div>
-        </div> -->
+        </div>
     </footer>
     <!-- Vendor JS-->
     <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
